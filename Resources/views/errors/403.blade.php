81e397de00d4c629bea2bd310cb8ef307535e438
marco76tv <marco.sottana@gmail.com>
2021-09-16 20:40:37 +0200

    .

--- Resources/views/errors/403.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('adm_theme::errors.minimal')
-
-@section('title', __('Forbidden'))
-@section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
--- Resources/views/errors/500.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('errors::minimal')
-
-@section('title', __('Server Error'))
-@section('code', '500')
-@section('message', $message ?? __('Server Error'))
